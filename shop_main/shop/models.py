7354ef21fdb7bcb4801dbce6061208713906df57
CLYDA <arian.zafarei2016@gmail.com>
2025-10-28 23:25:25 +0330

    Category model

--- shop_main/shop/models.py
+++ shop_main/shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.name
+
+
+# class Product(models.Model):
+#     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE , verbose_name='دسته بندی')
