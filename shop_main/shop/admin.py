71454803cd11fbb605b86a1c6101ea452b31136f
CLYDA <arian.zafarei2016@gmail.com>
2025-11-01 23:06:08 +0330

    Add image to Admin

--- shop_main/shop/admin.py
+++ shop_main/shop/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from .models improt *
+from .models import *
+
+
+class ImageInLine(admin.TabularInline):
+    model = Image
+    extra = 0
+
+class FeatureInLine(admin.TabularInline):
+    model = ProductFeature
+    extra = 0
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name')}
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'inventory', 'new_price', 'created', 'updated']
     list_filter = ['created', 'updated']
-    prepopulated_fields = {'slug': ('name')} 
+    prepopulated_fields = {'slug': ('name',)} 
+    inlines = [ImageInLine,FeatureInLine]
